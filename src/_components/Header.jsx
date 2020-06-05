e3523f098d446f16ca7dbd3aa876d25f2d30d61a
Anna Malik <annanmalik@hotmail.com>
2020-06-05 19:59:57 +0200

    styles #3

--- src/_components/Header.jsx
+++ src/_components/Header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import "./Header.scss";
-import Button from "@material-ui/core/Button";
 
 export class Header extends React.PureComponent {
     render() {
