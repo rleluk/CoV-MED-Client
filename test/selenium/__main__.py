9d403dd8d134bd64ceb5d55eb53d2f05204bf36f
Michal Pabjan <michal.pabjan@nokia.com>
2020-06-08 01:45:25 +0200

    Selenium testing

--- test/selenium/__main__.py
+++ test/selenium/__main__.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+import selenium
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -77,65 +78,233 @@ class PythonOrgSearch(unittest.TestCase):
 
     def test_client(self):
         self.driver.get("http://cov-med.herokuapp.com/signin")
-        print("before = {}".format(self.driver.current_url))
         self.driver.find_element_by_name("email").send_keys("ClientSeleniumTestAccount@mail.com")
         self.driver.find_element_by_name("password").send_keys("ClientseleniumTestPassword")
         sign_in_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/form/button/span[1]")
         sign_in_button.click()
-        print("after = {}".format(self.driver.current_url))
         time.sleep(3)
         self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/client/homepage")
 
         doctors_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[2]/a")
         doctors_button.click()
 
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/doctors")
+
         examinations_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[3]/a")
         examinations_button.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/examinations")
 
         patient_panel_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[4]/a")
         patient_panel_button.click()
 
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/client/homepage")
+
         examinations_history_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[5]/a")
         examinations_history_button.click()
 
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/client/examination-history")
+
         patient_panel_button_2 = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/a/button/span[1]")
         patient_panel_button_2.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/client/homepage")
 
         examinations_results_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[6]/a")
         examinations_results_button.click()
 
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/client/examination-results")
+
         patient_panel_button_2 = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/a/button/span[1]")
         patient_panel_button_2.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/client/homepage")
 
         arrange_examination_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[7]/a")
         arrange_examination_button.click()
 
-        time.sleep(2)
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/client/new-appointment")
+
+        time.sleep(1)
 
         select_city = Select(self.driver.find_element_by_xpath('//*[@id="select-city"]'))
         select_city.select_by_index(2)
 
-        time.sleep(2)
+        time.sleep(1)
 
         select_street = Select(self.driver.find_element_by_xpath('//*[@id="select-street"]'))
         select_street.select_by_index(1)        
 
-        time.sleep(2)
+        time.sleep(1)
 
         select_service = Select(self.driver.find_element_by_xpath('//*[@id="select-service"]'))
         select_service.select_by_index(5)
+
+        time.sleep(1)
+
+        select_doctor = Select(self.driver.find_element_by_xpath('//*[@id="select-doctor"]'))
+        select_doctor.select_by_index(1)
         
+        date_picker = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/form/div[5]/input")
+        date_picker.clear()
+
+        current_year = time.strftime("%Y")
+
+        self.driver.execute_script('document.getElementsByName("date").value = "2021-10-10"')
+
+        time.sleep(4)
+
+        time_picker = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/form/div[6]/select")
+        time_picker.send_keys("8:00")
+        # time_picker.send_keys("8:00").select_by_index(1)
+
+        time.sleep(2)
+
+        submit_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/form/button")
+        submit_button.click()
+
         patient_panel_button_2 = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/a/button/span[1]")
         patient_panel_button_2.click()
+
+        examinations_history_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[5]/a")
+        examinations_history_button.click()
+
+        time.sleep(3)
 
         log_out = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/button/span[1]")
         log_out.click()
 
         time.sleep(3)
 
+    def test_doctor(self):
+        self.driver.get("http://cov-med.herokuapp.com/signin")
+        self.driver.find_element_by_name("email").send_keys("DoctorSeleniumTestAccount@mail.com")
+        self.driver.find_element_by_name("password").send_keys("DoctorseleniumTestPassword")
+        sign_in_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/form/button/span[1]")
+        sign_in_button.click()
+        time.sleep(3)
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/doctor/homepage")
+
+        doctors_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[2]/a")
+        doctors_button.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/doctors")
+
+        examinations_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[3]/a")
+        examinations_button.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/examinations")
+
+        doctor_panel_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[4]/a")
+        doctor_panel_button.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/doctor/homepage")
+
+        appintments_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[5]/a")
+        appintments_button.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/doctor/appointments")
+
+        doctor_panel_button_2 = self.driver.find_element_by_xpath("  /html/body/div[1]/div/div[1]/div/a/button/span[1]")
+        doctor_panel_button_2.click()
+
+        patients_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[6]/a")
+        patients_button.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/doctor/patients")
+
+        doctor_panel_button_2 = self.driver.find_element_by_xpath("  /html/body/div[1]/div/div[1]/div/a/button/span[1]")
+        doctor_panel_button_2.click()
+
+        log_out_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/button/span[1]")
+        log_out_button.click()
+
+
+    def test_receptionist(self):
+        self.driver.get("http://cov-med.herokuapp.com/signin")
+        self.driver.find_element_by_name("email").send_keys("ReceptionistSeleniumTestAccount@mail.com")
+        self.driver.find_element_by_name("password").send_keys("ReceptionistleniumTestPassword")
+        sign_in_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/form/button/span[1]")
+        sign_in_button.click()
+        time.sleep(3)
+        
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/receptionist/homepage")
+
+        doctors_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[2]/a")
+        doctors_button.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/doctors")
+
+        examinations_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[3]/a")
+        examinations_button.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/examinations")
+
+        receptionist_panel_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[4]/a")
+        receptionist_panel_button.click()
+    
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/receptionist/homepage")
+
+        appointments_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[5]/a")
+        appointments_button.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/receptionist/appointments")
+
+        time.sleep(2)
+
+        rearange_appointments_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/table/tbody/tr[2]/td[4]/a/button/span[1]")
+        rearange_appointments_button.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/receptionist/postpone-appointment")
+
+        submit_rearange_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/form/button")
+        submit_rearange_button.click()
+
+        receptionist_panel_button_2 = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/a/button/span[1]")
+        receptionist_panel_button_2.click()
+
+        appointments_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[5]/a")
+        appointments_button.click()
+
+        time.sleep(2)
+
+        cancel_appointment = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/table/tbody/tr[3]/td[5]/button/span[1]")
+        cancel_appointment.click()
+
+        time.sleep(1)
+
+        deny_cancelation = self.driver.find_element_by_xpath("/html/body/div[3]/div/div/div/div/button[2]")
+        deny_cancelation.click()
+
+        receptionist_panel_button_2 = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/a/button/span[1]")
+        receptionist_panel_button_2.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/receptionist/homepage")
+
+        patients_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[1]/ul/li[6]/a")
+        patients_button.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/receptionist/patients")
+
+        time.sleep(1)
+
+        deactivate_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/table/tbody/tr[2]/td[3]/button/span[1]")
+        deactivate_button.click()
+
+        time.sleep(1)
+
+        deny_deactivation = self.driver.find_element_by_xpath("/html/body/div[3]/div/div/div/div/button[2]")
+        deny_deactivation.click()
+
+        log_out_button = self.driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/button/span[1]")
+        log_out_button.click()
+
+        self.assertEqual(self.driver.current_url, "http://cov-med.herokuapp.com/")
+
 
     @classmethod
-    def tearDownCadss(self):
+    def tearDownClass(self):
         self.driver.close()
 
 if __name__ == "__main__":
